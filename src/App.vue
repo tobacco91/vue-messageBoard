76630f7900918fe9cec3bfb17d4b130028fa7505
tobacco91 <991243902@qq.com>
2017-03-11 15:47:52 +0800

    vue留言板

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,50 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <p>{{information.name}}</p>
+    <p>{{information.time}}</p>
+    <ul>
+      <li v-for="message in messages">
+        <p v-text="message.name"></p>
+        <p v-text="message.message"></p>
+        <p v-text="message.time"></p>
+      </li>
+    </ul>
+    <textarea v-model="newMessage.message"></textarea>
+    <button type="button" @click="add()">提交</button>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
+  name: 'app',
+  data() {
+    return {
+        messages: [],
+        information : {
+            name : '胖砸',
+            time : new Date()
+        },
+        newMessage: {
+            name: '',
+            time: '',
+            message: ''
+        }
+    }
+  },
+  mounted :function() {
+    this.getInformation();
+  },
+  methods: {
+    add : function() {
+        console.log(this)
+        this.newMessage.time = new Date();
+        this.messages.push(this.newMessage)
+    },
+    getInformation : function() {
+        this.newMessage.name = this.information.name;
+    }
+  }
 }
 </script>
 
